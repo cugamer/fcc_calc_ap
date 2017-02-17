09fd89de5ce7b5c224c1c6f122a25ac4a01a84d2
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2017-02-17 13:13:34 -0600

    Add functions to add and remove from input objects

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 function inputConstructor() {
 	var input = {
-		val:         [],
-		opperations: []
+		vals:         [],
+		opps: []
 	}
 	return input;
 }
 
-function addValToInput(val, input) {
-	input.values.push(val);
+function addValToInput(vals, input) {
+	var big = new BigNumber(vals);
+	input.vals.push(big);
 }
 
 function remValFromInput(pos, input) {
-	input.values.splice(pos, 1);
+	return input.vals.splice(pos, 1)[0];
+}
+
+function addOpToInput(op, input) {
+	input.opps.push(op);
+}
+
+function removeOpFromInput(pos, input) {
+	return input.opps.splice(pos, 1)[0];
 }
 
 function addTwo(inputObj) {
-	var first = convertToBignum(inputObj.val[0]);
-	var second = convertToBignum(inputObj.val[1]);
+	var first = convertToBignum(inputObj.vals[0]);
+	var second = convertToBignum(inputObj.vals[1]);
 	return first.plus(second);
 }
 
 function subtractTwo(inputObj) {
-	var first = convertToBignum(inputObj.val[0]);
-	var second = convertToBignum(inputObj.val[1]);
+	var first = convertToBignum(inputObj.vals[0]);
+	var second = convertToBignum(inputObj.vals[1]);
 	return first.minus(second);
 }
 
 function multiplyTwo(inputObj) {
-	var first = convertToBignum(inputObj.val[0]);
-	var second = convertToBignum(inputObj.val[1]);
+	var first = convertToBignum(inputObj.vals[0]);
+	var second = convertToBignum(inputObj.vals[1]);
 	return first.times(second);
 }
 
 function divideTwo(inputObj) {
-	var first = convertToBignum(inputObj.val[0]);
-	var second = convertToBignum(inputObj.val[1]);
+	var first = convertToBignum(inputObj.vals[0]);
+	var second = convertToBignum(inputObj.vals[1]);
 	return first.dividedBy(second);
 }
 
--- test/spec/inputManagement.js
+++ test/spec/inputManagement.js
@@ -7,8 +7,55 @@
 		});
 
 		it('objects should have correct keys', function() {
-			expect(inputConstructor().val).toEqual(jasmine.any(Array));
-			expect(inputConstructor().opperations).toEqual(jasmine.any(Array));
+			expect(inputConstructor().vals).toEqual(jasmine.any(Array));
+			expect(inputConstructor().opps).toEqual(jasmine.any(Array));
 		});
 	});
+
+	describe('When updating an input objects values', function() {
+		var inputOne = inputConstructor();
+		describe('"addValToInput function', function() {
+			it('should add a BigNumber object to an input objects "vals" property', function() {
+				addValToInput(55, inputOne);
+				expect(inputOne.vals.length).toEqual(1);
+				expect(inputOne.vals[0]).toEqual(jasmine.any(BigNumber));
+				expect(inputOne.vals[0].c[0]).toEqual(55);
+
+				addValToInput("55", inputOne);
+				expect(inputOne.vals.length).toEqual(2);
+				expect(inputOne.vals[1]).toEqual(jasmine.any(BigNumber));
+				expect(inputOne.vals[1].c[0]).toEqual(55);
+			});
+		});
+
+		describe('"remValFromInput" function', function() {
+			it('should remove items from the input objects "val" property', function() {
+				var removedOne = remValFromInput(1, inputOne);
+				expect(removedOne).toEqual(jasmine.any(BigNumber));
+				expect(removedOne.c[0]).toEqual(55);
+				expect(inputOne.vals.length).toEqual(1);
+			});
+		});
+
+		describe('"addOpToInput"', function() {
+			it('should add a string with the required opperation to an input objects "opps" property', function() {
+				addOpToInput("+", inputOne);
+				expect(inputOne.opps[0]).toEqual("+");
+				expect(inputOne.opps.length).toEqual(1);
+				addOpToInput("/", inputOne);
+				expect(inputOne.opps[1]).toEqual("/");
+				expect(inputOne.opps.length).toEqual(2);
+			});
+		});
+
+		describe('"removeOpFromInput"', function() {
+			it('should remove the desired string from the input objects "opps" property and return that value', function() {
+				// var remOpOne = ;
+				expect(removeOpFromInput(0, inputOne)).toEqual("+");
+				expect(inputOne.opps.length).toEqual(1);
+			})
+		})
+	});
+
+
 })();
